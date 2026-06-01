4e7b7a20a938905472532c79c1fe292d8ae774db
weichenyi <771038325@qq.com>
2026-06-01 11:59:27 +0800

    feat: 添加ICP备案号和免责声明到前端底部
    - 添加粤ICP备2025440449号备案号链接至工信部官网
    - 添加免责声明：本站仅供个人学习开发使用，不涉及任何商业利益及金钱收益

--- vite-project/src/components/Footer/index.tsx
+++ vite-project/src/components/Footer/index.tsx
@@ -9,6 +9,16 @@ const Footer: React.FC = () => {
   return (
     <AntFooter className={styles.footer}>
       <div className={styles.content}>
+        <div className={styles.bottom}>
+          <div className={styles.links}>
+            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
+              粤ICP备2025440449号
+            </a>
+          </div>
+          <Text type="secondary" style={{ fontSize: 12 }}>
+            本站仅供个人学习开发使用，不涉及任何商业利益及金钱收益
+          </Text>
+        </div>
         <Text type="secondary">
           © 2025 智慧医疗平台. All rights reserved.
         </Text>
